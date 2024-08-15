71e8031f091038a859ad3b0b79268cab02e834e4
catalinaschiaffarino <catalinaschiaffarinomartinez@gmail.com>
2024-08-15 19:25:43 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,10 +1,20 @@
-document.addEventListener("DOMContentLoaded", (event)=>{
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
     let loginBtn = document.getElementById("loginBtn");
-    loginBtn.addEventListener("click", ()=>{
+    loginBtn.addEventListener("click", () => {
         let usuario = document.getElementById("usuario").value;
         let contraseña = document.getElementById("contraseña").value;
-        if (usuario!="" && contraseña!=""){
+        localStorage.setItem("usuario", JSON.stringify(usuario));
+        localStorage.setItem("contraseña", JSON.stringify(contraseña));
+
+
+        let ObjUsuario = JSON.parse(localStorage.getItem("usuario"));
+        let ObjContraseña = JSON.parse(localStorage.getItem("contraseña"));
+
+
+        if (usuario != "" && contraseña != "") {
             window.location.href = "index.html";
-        };
+        }
     });
 });
